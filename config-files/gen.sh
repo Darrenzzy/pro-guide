c43d67062a7be27efa0b3e04c5a9faab6e6341a9
darren <zzyphp@gmail.com>
2021-05-10 23:56:41 +0800

    [update] format config

--- config-files/gen.sh
+++ config-files/gen.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-cp ~/.bash_profile ./bash_profile
+case $1 in
+set)
+    cp ~/.bash_profile ./bash_profile
+    ;;
+get)
+    cp ./bash_profile ~/.bash_profile
+    echo
+    ;;
 
+# Chrome | chrome)
+#     echo "Cool!!! It's for pro users. Amazing Choice."
+#     echo
+#     ;;
+*)
+    echo -n "unknown"
+    echo
+    ;;
+esac
